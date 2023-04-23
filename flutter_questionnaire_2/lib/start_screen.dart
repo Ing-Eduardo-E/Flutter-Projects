677efc5137dfe7962a3ddc52fd5fb899d0864b32
Ing. Eduardo E. Enríquez R <rekineke@hotmail.com>
2023-04-22 22:29:14 -0500

    renderizacion y cambio de pantallas con asignacion de widgets a las variables

--- flutter_questionnaire_2/lib/start_screen.dart
+++ flutter_questionnaire_2/lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen(void Function() startQuiz, {super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +23,7 @@ class StartScreen extends StatelessWidget {
         ),
         const SizedBox(height: 30),
         OutlinedButton.icon(
-          onPressed: () {},
+          onPressed: startQuiz,
           style: OutlinedButton.styleFrom(
             foregroundColor: const Color.fromARGB(255, 242, 227, 250),
           ),
