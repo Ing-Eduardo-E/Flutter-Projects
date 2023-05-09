e92059de38d26ed9d0a38c7f87cb18b4369c64d8
Ing. Eduardo E. Enríquez <rekineke@hotmail.com>
2023-05-08 22:27:23 -0500

    eliminar elementos de la lista y reestablecerlos, cambio de apariencia de la App

--- flutter_presupuesto_3/lib/widgets/expenses.dart
+++ flutter_presupuesto_3/lib/widgets/expenses.dart
@@ -26,6 +26,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
@@ -37,8 +38,40 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Gasto eliminado'),
+        action: SnackBarAction(
+          label: 'Deshacer',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No hay gastos registrados'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Presupuesto personal'),
@@ -52,7 +85,9 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           const Text('El gráfico va aquí'),
-          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+          Expanded(
+            child: mainContent,
+          ),
         ],
       ),
     );
